8ab36e104caeadd2bb50d28cd61891630c31ce78
Anichang <20516565+mfp20@users.noreply.github.com>
2020-04-15 23:20:17 +0200

    added start.*.sh scripts to ease testing

---
+++ start.klippy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pkill -f /home/user/klippy-env/bin/python
+rm klippy.log
+
+CFG="printer.cfg"
+GCODE=""
+
+if [ "$1" == "simple" ];then
+	CFG="simple.printer.cfg"
+elif [ "$1" == "gcode" ];then
+	GCODE=" -i test/klippy/move.gcode"
+fi
+
+
+/home/user/klippy-env/bin/python klippy/klippy.py ${CFG} -l klippy.log -v ${GCODE} &
+
+sleep 1
+
+tail -f klippy.log
+
